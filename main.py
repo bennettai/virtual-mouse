2d22fb0e4f98746a8228f06878aac59b54b29f91
pdevanshi2003 <pdevanshi2003@gmail.com>
2023-09-04 03:08:11 +0530

    Added Gesture Detection Model

--- main.py
+++ main.py
@@ -1,38 +1,78 @@
 import cv2
+import numpy as np
+import mediapipe as mp
+import tensorflow as tf
+from keras.saving import load_model
 
-# Open the webcam (you can specify the camera index or video file path)
+# initialize mediapipe
+mpHands = mp.solutions.hands
+hands = mpHands.Hands(max_num_hands=1, min_detection_confidence=0.7)
+mpDraw = mp.solutions.drawing_utils
+
+# Load the gesture recognizer model
+model = load_model('mp_hand_gesture')
+
+# Load class names
+f = open('gesture.names', 'r')
+classNames = f.read().split('\n')
+f.close()
+print(classNames)
+
+
+# Initialize the webcam
 cap = cv2.VideoCapture(0)
 
-# Check if the webcam opened successfully
-if not cap.isOpened():
-    print("Error: Could not open webcam")
-    exit()
-
 while True:
-    # Read a frame from the webcam
-    ret, frame = cap.read()
+    # Read each frame from the webcam
+    _, frame = cap.read()
 
-    if not ret:
-        print("Error: Could not read frame")
+    x, y, c = frame.shape
+
+    # Flip the frame vertically
+    frame = cv2.flip(frame, 1)
+    framergb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+
+    # Get hand landmark prediction
+    result = hands.process(framergb)
+    
+    className = ''
+
+    # post process the result
+    if result.multi_hand_landmarks:
+        landmarks = []
+        for handslms in result.multi_hand_landmarks:
+            for lm in handslms.landmark:
+                # print(id, lm)
+                lmx = int(lm.x * x)
+                lmy = int(lm.y * y)
+
+                landmarks.append([lmx, lmy])
+
+            # Drawing landmarks on frames
+            mpDraw.draw_landmarks(frame, handslms, mpHands.HAND_CONNECTIONS)
+
+            # Predict gesture
+            prediction = model.predict([landmarks])
+      
+            classID = np.argmax(prediction)
+            className = classNames[classID]
+
+            if className == 'thumb down':
+                # Write pyautogui logic
+                pass
+
+
+    # show the prediction on the frame
+    cv2.putText(frame, className, (10, 50), cv2.FONT_HERSHEY_SIMPLEX, 
+                   1, (0,0,255), 2, cv2.LINE_AA)
+
+    # Show the final output
+    cv2.imshow("Output", frame) 
+
+    if cv2.waitKey(1) == ord('q'):
         break
 
-    # Add the "Hello, World!" text to the frame
-    text = "Hello, World!"
-    font = cv2.FONT_HERSHEY_SIMPLEX
-    position = (20, 50)
-    font_scale = 1
-    font_color = (0, 255, 0)  # Green color in BGR
-    line_thickness = 2
-
-    cv2.putText(frame, text, position, font, font_scale, font_color, line_thickness)
-
-    # Display the frame with the text
-    cv2.imshow('Webcam', frame)
-
-    # Exit the loop when the 'q' key is pressed
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
-
-# Release the webcam and close the OpenCV window
+# release the webcam and destroy all active windows
 cap.release()
+
 cv2.destroyAllWindows()
