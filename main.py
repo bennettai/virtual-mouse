9ef2437f19d35982b27991c10dfdb13c10c71cd9
pdevanshi2003 <pdevanshi2003@gmail.com>
2023-09-04 01:50:11 +0530

    changed webcam code

--- main.py
+++ main.py
@@ -1,12 +1,38 @@
 import cv2
 
+# Open the webcam (you can specify the camera index or video file path)
 cap = cv2.VideoCapture(0)
 
-# Check if the webcam is opened correctly
+# Check if the webcam opened successfully
 if not cap.isOpened():
-    raise IOError("Cannot open webcam")
+    print("Error: Could not open webcam")
+    exit()
 
 while True:
+    # Read a frame from the webcam
     ret, frame = cap.read()
-    frame = cv2.resize(frame, None, fx=0.5, fy=0.5, interpolation=cv2.INTER_AREA)
-    cv2.imshow('Input', frame)
+
+    if not ret:
+        print("Error: Could not read frame")
+        break
+
+    # Add the "Hello, World!" text to the frame
+    text = "Hello, World!"
+    font = cv2.FONT_HERSHEY_SIMPLEX
+    position = (20, 50)
+    font_scale = 1
+    font_color = (0, 255, 0)  # Green color in BGR
+    line_thickness = 2
+
+    cv2.putText(frame, text, position, font, font_scale, font_color, line_thickness)
+
+    # Display the frame with the text
+    cv2.imshow('Webcam', frame)
+
+    # Exit the loop when the 'q' key is pressed
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# Release the webcam and close the OpenCV window
+cap.release()
+cv2.destroyAllWindows()
